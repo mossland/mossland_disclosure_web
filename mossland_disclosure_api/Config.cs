8094d617c5fb7e6c33ffd1e7934ad14a233fc23b
Wooram Son <wooram.son@gmail.com>
2023-01-05 18:15:09 +0000

    upsert market_data

--- mossland_disclosure_api/Config.cs
+++ mossland_disclosure_api/Config.cs
@@ -47,43 +47,36 @@ namespace mossland_disclosure_api
         {
             while (true)
             {
-                int coingecko = 0;
-                int coinmarketcap = 0;
-                int mossland = 0;
-
-                try
-                {
-                    coingecko = QueryCirculatingSupplyCoinGecko();
+                try {
+                    QueryCirculatingSupplyCoinGecko();
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
+                catch (Exception ex) {
+                    Console.WriteLine("[ERR] QueryCirculatingSupplyCoinGecko() - " + ex.Message);
                 }
 
-                try
-                {
-                    coinmarketcap = QueryCirculatingSupplyCoinmarketcap();
+                try {
+                    QueryCirculatingSupplyCoinmarketcap();
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
+                catch (Exception ex) {
+                    Console.WriteLine("[ERR] QueryCirculatingSupplyCoinmarketcap() - " + ex.Message);
                 }
 
-                try
-                {
-                    mossland = QueryCirculatingSupplyMossland();
+                try {
+                    QueryCirculatingSupplyMossland();
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
+                catch (Exception ex) {
+                    Console.WriteLine("[ERR] QueryCirculatingSupplyMossland() - " + ex.Message);
                 }
 
-                Thread.Sleep(1000 * 3);
+                Thread.Sleep(1000 * 60 * 10);   // 10 min
             }
         }
 
-        private int QueryCirculatingSupplyCoinGecko()
+        private void QueryCirculatingSupplyCoinGecko()
         {
+            if (db == null)
+                return;
+
             var URL = new UriBuilder("https://api.coingecko.com/api/v3/coins/mossland?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false");
 
             var client = new WebClient();
@@ -91,28 +84,48 @@ namespace mossland_disclosure_api
             string json = client.DownloadString(URL.ToString());
 
             JObject jObject = JObject.Parse(json);
-            return jObject["market_data"]["circulating_supply"].ToObject<int>();
+            double marketcap_usd = jObject["market_data"]["market_cap"]["usd"].ToObject<double>();
+            double marketcap_krw = jObject["market_data"]["market_cap"]["krw"].ToObject<double>();
+            double circulating_supply = jObject["market_data"]["circulating_supply"].ToObject<double>();
+
+            db.UpsertMarketData("coingecko_marketcap_usd", marketcap_usd);
+            db.UpsertMarketData("coingecko_marketcap_krw", marketcap_krw);
+            db.UpsertMarketData("coingecko_circulating_supply", circulating_supply);
         }
 
-        private int QueryCirculatingSupplyCoinmarketcap()
+        private void QueryCirculatingSupplyCoinmarketcap()
         {
             var URL = new UriBuilder("https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest");
 
             var queryString = HttpUtility.ParseQueryString(string.Empty);
             queryString["symbol"] = "MOC";
-
+            queryString["convert"] = "USD";
             URL.Query = queryString.ToString();
 
+            // request USD
             var client = new WebClient();
             client.Headers.Add("X-CMC_PRO_API_KEY", Config.Instance.coinmarketcap_key);
             client.Headers.Add("Accepts", "application/json");
             string json = client.DownloadString(URL.ToString());
 
             JObject jObject = JObject.Parse(json);
-            return jObject["data"]["MOC"][0]["circulating_supply"].ToObject<int>();
+            double market_cap_usd = jObject["data"]["MOC"][0]["quote"]["USD"]["market_cap"].ToObject<double>();
+            double circulating_supply = jObject["data"]["MOC"][0]["circulating_supply"].ToObject<double>();
+
+            db.UpsertMarketData("coinmarketcap_marketcap_usd", market_cap_usd);
+            db.UpsertMarketData("coinmarketcap_circulating_supply", circulating_supply);
+
+            // request KRW
+            queryString["convert"] = "KRW";
+            URL.Query = queryString.ToString();
+            json = client.DownloadString(URL.ToString());
+            jObject = JObject.Parse(json);
+            double market_cap_krw = jObject["data"]["MOC"][0]["quote"]["KRW"]["market_cap"].ToObject<double>();
+
+            db.UpsertMarketData("coinmarketcap_marketcap_krw", market_cap_krw);
         }
 
-        private int QueryCirculatingSupplyMossland()
+        private void QueryCirculatingSupplyMossland()
         {
             var URL = new UriBuilder("https://api.moss.land/MOC/info");
 
@@ -121,7 +134,16 @@ namespace mossland_disclosure_api
             string json = client.DownloadString(URL.ToString());
 
             JArray jArray = JArray.Parse(json);
-            return jArray[0]["circulatingSupply"].ToObject<int>();
+            double market_cap_usd = jArray[1]["marketCap"].ToObject<double>();
+            double market_cap_krw = jArray[0]["marketCap"].ToObject<double>();
+            double circulating_supply = jArray[0]["circulatingSupply"].ToObject<double>();
+            double max_supply = jArray[0]["maxSupply"].ToObject<double>();
+
+
+            db.UpsertMarketData("mossland_marketcap_usd", market_cap_usd);
+            db.UpsertMarketData("mossland_marketcap_krw", market_cap_krw);
+            db.UpsertMarketData("mossland_circulating_supply", circulating_supply);
+            db.UpsertMarketData("mossland_max_supply", max_supply);
         }
 
 
--- mossland_disclosure_api/Database.cs
+++ mossland_disclosure_api/Database.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Ocsp;
+using Org.BouncyCastle.Utilities.Collections;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 using System.Threading;
 
@@ -20,6 +24,73 @@ namespace mossland_disclosure_api
             this.Scheme = scheme;
 
             connstring = string.Format("Server=localhost; database={0}; UID={1}; password={2}", Scheme, UserID, Password);
+        }
+
+        public bool UpsertMarketData(string market_type, double val)
+        {
+            bool ret = false;
+            MySqlConnection connection = new MySqlConnection(connstring);
+
+            try
+            {
+                connection.Open();
+
+                MySqlCommand comm = connection.CreateCommand();
+                comm.CommandText = "INSERT INTO market_data(market_type, number) VALUES(?market_type, ?number) ON DUPLICATE KEY UPDATE number=?number, timestamp=CURRENT_TIMESTAMP";
+                comm.Parameters.AddWithValue("?market_type", market_type);
+                comm.Parameters.AddWithValue("?number", val);
+
+                if (comm.ExecuteNonQuery() > 0)
+                    ret = true;
+
+                comm.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[ERR] InsertMarketData() - " + ex.Message);
+            }
+
+            connection.Close();
+            return ret;
+        }
+
+        public (double, DateTime) SelectMarketData(string market_type)
+        {
+            double val = -1;
+            DateTime timestamp = DateTime.UtcNow;
+
+            MySqlConnection connection = new MySqlConnection(connstring);
+
+            try
+            {
+                connection.Open();
+
+                MySqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT * FROM market_data WHERE market_type=?market_type";
+                cmd.Parameters.AddWithValue("?market_type", market_type);
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    val = reader.GetDouble("number");
+                    timestamp = ConvertToUTC(reader.GetDateTime("timestamp"));
+                }
+
+                reader.Close();
+                cmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[ERR] SelectMarketData() - " + ex.Message);
+            }
+
+            connection.Close();
+            return (val, timestamp);
+        }
+
+        public static DateTime ConvertToUTC(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, dateTime.Second, DateTimeKind.Utc);
         }
     }
 }
