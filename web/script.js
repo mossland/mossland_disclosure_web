6b105321358425be1ced4f00fce079e78f9e60e6
Chohyuntae <chohyuntae@realityreflection.com>
2023-08-16 11:46:31 +0900

    add locked-up wallet

--- web/script.js
+++ web/script.js
@@ -116,6 +116,7 @@ $.lang.ko = {
     97: '값(WMOC)',
     98: '1) WMOC 총 발행량에서 락업된 WMOC 수량을 제외한 실제 WMOC 유통량',
     99: '2) 유통 가능한 MOC와 WMOC의 합산 값으로 MOC 유통량을 초과하면 안됨',
+    100: '락업 지갑 (swap.moss.land)'
 };
 
 $.lang.en = {
@@ -227,6 +228,7 @@ $.lang.en = {
     97: 'Value(WMOC)',
     98: '1) The circulating supply of WMOC, excluding the quantity of locked-up WMOC, from the total supply of WMOC.',
     99: '2) The combined value of tradable MOC and WMOC in circulation must not exceed the circulating supply of MOC.',
+    100: 'Locked-up Wallet (swap.moss.land)'
 };
 
 
